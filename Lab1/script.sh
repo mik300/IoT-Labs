018e768a47a19a93b1da0487bd5a36cfc2f74112
Michael <emichael283@gmail.com>
2023-11-11 13:59:05 +0100

    Added results folder

--- Lab1/script.sh
+++ Lab1/script.sh
@@ -9,9 +9,11 @@ echo $file
 read -p "Select the file that describes the PSM of the resource: " psm
 echo $psm
 
+rm results/workload_1_result.txt
+rm results/workload_2_result.txt
 
 for (( c=$TOin; c<=$TOend; c=c+1 )) do 
     echo "Simulation when TO is $c"
-   ./dpm-simulator/dpm_simulator -t $c -psm ./dpm-simulator/example/$psm -wl ./workloads/$file
+   ./dpm-simulator/dpm_simulator -t $c -psm ./dpm-simulator/example/${psm}.txt -wl ./workloads/${file}.txt | tail -1 | grep -Eo "[0-9]+\.[0-9]+J$" >> results/${file}_result.txt
    echo "-------------------------------------------------------------------------------------"
 done
