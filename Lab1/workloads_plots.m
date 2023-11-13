d4a535870fa8f34706a037157e89b72bba423b5d
Michael <emichael283@gmail.com>
2023-11-13 09:46:16 +0100

    Added workload2 plot

--- Lab1/workloads_plots.m
+++ Lab1/workloads_plots.m
@@ -1,21 +1,22 @@
 A = importdata('workloads/workload_1.txt');
+B = importdata('workloads/workload_2.txt');
 ENERGYWL1 = importdata('results/workload_1_results.txt');
 ENERGYWL2 = importdata('results/workload_2_results.txt');
 TO = importdata('results/TO.txt');
 
-tiledlayout(1,2)
+
 figure(1)
+tiledlayout(1,2)
 nexttile
 plot(TO,ENERGYWL1)
-title("Energy consumption of workload 1 vs. time out")
-xlabel('Time out time (in ms)') 
+title("Energy consumption of workload 1 vs. Timeout (TO)")
+xlabel('TO (in ms)') 
 ylabel('Energy dissipated')
 grid
 nexttile
 plot(TO,ENERGYWL2)
-title("Energy consumption of workload 2 vs. time out")
-ylabel('Energy consumption of workload 2 vs. time out')
-xlabel('Time out time (in ms)') 
+title("Energy consumption of workload 2 vs. Timeout (TO)")
+xlabel('TO (in ms)') 
 ylabel('Energy dissipated')
 grid
 
@@ -51,4 +52,46 @@ end
 Y = [Y 0];
 figure(2)
 plot(X, Y)
+title("Workload 1")
+xlabel('Time (in ms)') 
+ylabel('Active/Inactive')
+grid
+
+
+X1 = []; 
+Y1 = [1];
+X = [3]
+%this for loop is the same as the previous one but using the data of
+%workload_2.txt instead of workload_1.txt
+for j = 1:size(B,1) 
+    x1 = B(j, 1);    
+    y1 = B(j, 2);    
+    z1 = x1+y1;        
+    X1 = [X1 x1 x1];    
+    X1 = [X1 z1 z1];    
+    
+    
+    if y1 == 0
+         a1 = 0;         
+         Y1 = [Y1 a1, a1];   
+    else
+         a1 = 1; 
+         Y1 = [Y1 a1, a1];       
+    end 
+    
+    if j < size(B,1)
+        if  z1 == B(j+1, 1)   
+            Y1 = [Y1 a1, a1];   
+        else
+            a1 = 0;         
+            Y1 = [Y1 a1, a1];   
+        end
+    end
+end
+Y1 = [Y1 0];
+figure(3)
+plot(X1, Y1)
+title("Workload 2")
+xlabel('Time (in ms)') 
+ylabel('Active/Inactive')
 grid
