5c10b37ef948763b13a50fcea65acb1e31b7597b
juanjse13 <restrepotorojuanjose@gmail.com>
2023-12-22 08:47:11 +0100

    ADD: Add new files and modification to try several values

---
+++ Lab2/plots.m
@@ -0,0 +1,6 @@
+stem(Average_distortion, Average_average_power_saving);
+title("Average power vs Average distortion")
+ylabel('Average power')
+xlabel('Average distortion') 
+grid
+
---
+++ Lab2/saving_perc.m
@@ -0,0 +1,3 @@
+function savings = saving_perc(original_power,modified_power)
+savings = ((original_power - modified_power) / original_power) * 100;
+end
