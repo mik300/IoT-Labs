384d6f118809f3795af9a580eef6d471e94f4704
juanjse13 <restrepotorojuanjose@gmail.com>
2024-02-21 16:21:49 +0100

    Fix: fix function of brightness scaling

--- Lab2/brightness_scaling.m
+++ Lab2/brightness_scaling.m
@@ -1,6 +1,9 @@
 function  mod_rgb = brightness_scaling(img_rgb, b)
-mod_rgb = img_rgb;
-mod_rgb(:, :, 1) = img_rgb(:, :, 1) + b;
-mod_rgb(:, :, 2) = img_rgb(:, :, 2) + b;
-mod_rgb(:, :, 3) = img_rgb(:, :, 3) + b;
+pre_mod_rgb = rgb2hsv(img_rgb)
+pre_mod_rgb(:, :, 1) = pre_mod_rgb(:, :, 1) + b;
+pre_mod_rgb(:, :, 2) = pre_mod_rgb(:, :, 2) + b;
+pre_mod_rgb(:, :, 3) = pre_mod_rgb(:, :, 3) + b;
+mod_img = hsv2rgb(pre_mod_img);
+mod_img = mod_img * 255;
+mod_img = cast(mod_img, "uint8");
 end
